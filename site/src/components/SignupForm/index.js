84958deaef41746e018cf457138bac64aa89db42
Santosh Kaluskar <dtshbl@gmail.com>
2022-07-07 12:38:58 +0530

    Reformatting for lint errors
    Signed-off-by: Santosh Kaluskar <dtshbl@gmail.com>

--- site/src/components/SignupForm/index.js
+++ site/src/components/SignupForm/index.js
@@ -1,305 +1,189 @@
-import { useState, useEffect } from 'react';
-import { Field, Formik, Form } from 'formik';
-import axios from 'axios';
-import MesheryText from '../../assets/images/meshery-light-text.svg';
-import SignupFormWrapper from './signupform.style.js';
-import DiscussCallout from '../Discuss-Callout';
-import mesheryCloud from '../../assets/images/meshery-cloud.png';
+import { useState, useEffect } from "react";
+import { Field, Formik, Form } from "formik";
+import axios from "axios";
+import MesheryText from "../../assets/images/meshery-light-text.svg"
+import SignupFormWrapper from "./signupform.style.js";
+import DiscussCallout from "../Discuss-Callout";
+import mesheryCloud from "../../assets/images/meshery-cloud.png";
 
 const SignupForm = () => {
-	// Form values
-	const [memberFormOne, setMemberFormOne] = useState({});
 
-	const [formSubmitted, setSubmit] = useState(false);
+  // Form values
+  const [memberFormOne, setMemberFormOne] = useState({});
 
-	const [validateAccounts, setValidateAccounts] = useState(false);
-	const [validateRole, setValidateRole] = useState(false);
-	const [email, setEmail] = useState('');
-	const [firstname, setFirstName] = useState('');
-	const [lastname, setLastName] = useState('');
-	const [org, setOrg] = useState('');
-	const [occupation, setOccupation] = useState('');
-	const [role, setRole] = useState('');
-	const [google, setGoogleAccount] = useState('');
-	const [github, setGithubAccount] = useState('');
-	const [twitter, setTwitterAccount] = useState('');
-	const [linkedin, setLinkedinAccount] = useState('');
+  const [formSubmitted, setSubmit] = useState(false);
 
-	const errorAccounts = 'Please provide at least one account';
-	const errorRole = 'Please select role as applicable';
+  const [validateAccounts, setValidateAccounts] = useState(false);
+  const [validateRole, setValidateRole] = useState(false);
+  const [email, setEmail] = useState("");
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [org, setOrg] = useState("");
+  const [occupation, setOccupation] = useState("");
+  const [role, setRole] = useState("");
+  const [google, setGoogleAccount] = useState("");
+  const [github, setGithubAccount] = useState("");
+  const [twitter, setTwitterAccount] = useState("");
+  const [linkedin, setLinkedinAccount] = useState("");
 
-	useEffect(() => {
-		if (formSubmitted) {
-			axios.post('https://hook.us1.make.com/gguommoyd14634ur9xs7l37widuoa7e9', {
-				memberFormOne,
-			});
-			window.scrollTo({
-				top: 800,
-				left: 0,
-				behavior: 'smooth',
-			});
-		}
-	}, [formSubmitted, memberFormOne]);
+  const errorAccounts = "Please provide at least one account";
+  const errorRole = "Please select role as applicable";
 
-	const PlayFormComponent = () => {
-		return (
-			<div className='form-page'>
-				<div className='desc-text'>
-					<p>
-						Sign up for the early access of Meshery Playground today. Your
-						request for access will be processed as quickly as possible. Due to
-						the large influx of program participation requests, it may take some
-						time before system access is granted. In the meantime to help you
-						familiarize with Meshery, the Layer5 team will send you additional
-						information about the early access program.
-					</p>
-					<img
-						src={mesheryCloud}
-						alt='Meshery Homepage'
-						className='meshery-home'
-					/>
-					<DiscussCallout />
-				</div>
-				<div className='form-container'>
-					<h3 className='form-title'>Apply For the Beta Program</h3>
-					<Formik
-						initialValues={{
-							firstname: firstname,
-							lastname: lastname,
-							email: email,
-							occupation: occupation,
-							org: org,
-							google: google,
-							github: github,
-							twitter: twitter,
-							linkedin: linkedin,
-							role: role,
-							form: 'play',
-						}}
-						onSubmit={values => {
-							if (
-								(values.google ||
-									values.github ||
-									values.twitter ||
-									values.linkedin) &&
-								values.role
-							) {
-								setMemberFormOne(values);
-								setSubmit(true);
-							} else {
-								if (!values.role) {
-									setValidateRole(true);
-								} else {
-									setValidateRole(false);
-								}
-								if (
-									!(
-										values.google ||
-										values.github ||
-										values.twitter ||
-										values.linkedin
-									)
-								) {
-									setValidateAccounts(true);
-								} else {
-									setValidateAccounts(false);
-								}
-								setFirstName(values.firstname);
-								setEmail(values.email);
-								setLastName(values.lastname);
-								setOccupation(values.occupation);
-								setOrg(values.org);
-								setGoogleAccount(values.google);
-								setTwitterAccount(values.twitter);
-								setGithubAccount(values.github);
-								setLinkedinAccount(values.linkedin);
-								setRole(values.role);
-							}
-						}}>
-						<Form className='form1' method='post'>
-							<div className='form-group'>
-								<label htmlFor='firstname' className='form-field'>
-									First Name <span className='required-sign'>*</span>
-								</label>
-								<Field
-									type='text'
-									className='text-field'
-									id='firstname'
-									name='firstname'
-									maxLength='32'
-									pattern='[A-Za-z]{1,32}'
-									required
-								/>
-							</div>
-							<div className='form-group'>
-								<label htmlFor='lastname' className='form-field'>
-									Last Name <span className='required-sign'>*</span>
-								</label>
-								<Field
-									type='text'
-									className='text-field'
-									id='lastname'
-									name='lastname'
-									maxLength='32'
-									pattern='[A-Za-z]{1,32}'
-									required
-								/>
-							</div>
-							<div className='form-group'>
-								<label htmlFor='email' className='form-field'>
-									Email Address <span className='required-sign'>*</span>
-								</label>
-								<Field
-									type='email'
-									className='text-field'
-									id='email'
-									name='email'
-									pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
-									required
-								/>
-							</div>
-							<div className='form-group'>
-								<label htmlFor='occupation' className='form-field'>
-									Occupation / Title <span className='required-sign'>*</span>
-								</label>
-								<Field
-									type='text'
-									className='text-field'
-									id='occupation'
-									name='occupation'
-								/>
-							</div>
-							<div className='form-group'>
-								<label htmlFor='org' className='form-field'>
-									Org / Company / School{' '}
-									<span className='required-sign'>*</span>
-								</label>
-								<Field type='text' className='text-field' id='org' name='org' />
-							</div>
-							<div className='accounts'>
-								<label className='form-field'>Account(s) to Connect</label>
-								<p>
-									Choose between Twitter, Google, LinkedIn, and GitHub, provide
-									the username/handle of your user account for your preferred
-									identity provider. When enrolled, participants will receive a
-									free Meshery Cloud account and have full access to the Meshery
-									Playground early access program for each of the following user
-									accounts that you provide. Please provide at least one
-									account.
-								</p>
-								{validateAccounts && (
-									<p style={{ margin: '0px', color: 'red' }}>{errorAccounts}</p>
-								)}
-								<div className='accounts_group'>
-									<label htmlFor='google' className='form-name'>
-										Google
-									</label>
-									<Field
-										type='email'
-										className='text-field'
-										id='google'
-										name='google'
-										pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
-									/>
-									<label htmlFor='github' className='form-name'>
-										GitHub
-									</label>
-									<Field
-										type='text'
-										className='text-field'
-										id='github'
-										name='github'
-									/>
-									<label htmlFor='twitter' className='form-name'>
-										Twitter
-									</label>
-									<Field
-										type='text'
-										className='text-field'
-										id='twitter'
-										name='twitter'
-									/>
-									<label htmlFor='linkedin' className='form-name'>
-										Linkedin
-									</label>
-									<Field
-										type='text'
-										className='text-field'
-										id='linkedin'
-										name='linkedin'
-									/>
-								</div>
-							</div>
-							<div className='form-group'>
-								<label htmlFor='role' className='form-field'>
-									What role best identifies you?{' '}
-									<span className='required-sign'>*</span>
-								</label>
-							</div>
-							{validateRole && (
-								<p style={{ margin: '0px', color: 'red', fontSize: '12px' }}>
-									{errorRole}
-								</p>
-							)}
-							<div className='form-select' aria-labelledby='select'>
-								<Field as='select' name='role'>
-									<option>Select your role</option>
-									<option value='Architect'>Architect</option>
-									<option value='Business Operations'>
-										Business Operations
-									</option>
-									<option value='Developer'>Developer</option>
-									<option value='DevOps'>DevOps</option>
-									<option value='Executive'>Executive</option>
-									<option value='IT Operations'>IT Operations</option>
-									<option value='Media'>Media</option>
-									<option value='Product Manager'>Product Manager</option>
-									<option value='Professor'>Professor</option>
-									<option value='Sales Marketing'>Sales Marketing</option>
-									<option value='Student'>Student</option>
-									<option value='Other'>Other</option>
-								</Field>
-							</div>
-							<button type='submit' className='submit-btn'>
-								Submit
-							</button>
-						</Form>
-					</Formik>
-				</div>
-			</div>
-		);
-	};
+  useEffect(() => {
+    if (formSubmitted) {
+      axios.post("https://hook.us1.make.com/gguommoyd14634ur9xs7l37widuoa7e9", {
+        memberFormOne,
+      });
+      window.scrollTo({
+        top: 800,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+  }, [formSubmitted, memberFormOne]);
 
-	const ThankYou = () => {
-		return (
-			<>
-				<div className='thankyou-box'>
-					<h2>
-						Thank you for your interest in Meshery Playground early access
-						program!
-					</h2>
-					<p>
-						You are now signed up for the Meshery Playground early access
-						program and your position on the waiting list is confirmed. Please
-						wait patiently for a response from the Meshery team.
-					</p>
-					<p>
-						If you have any questions in the meantime, please send an email to{' '}
-						<a href='mailto:meshmap@layer5.io'>meshmap@layer5.io</a>.
-					</p>
-					<h3 className='white'>
-						- Team <img src={MesheryText} alt='Meshery' />
-					</h3>
-				</div>
-			</>
-		);
-	};
 
-	return (
-		<SignupFormWrapper>
-			{!formSubmitted ? <PlayFormComponent /> : <ThankYou />}
-		</SignupFormWrapper>
-	);
+  const PlayFormComponent = () => {
+    return (
+      <div className= "form-page">
+        <div className="desc-text">
+          <p>
+            Sign up for the early access of Meshery Playground today. Your request for access will be processed as quickly as possible. Due to the large influx of program participation requests, it may take some time before system access is granted. In the meantime to help you familiarize with Meshery, the Layer5 team will send you additional information about the early access program.
+          </p>
+          <img src={mesheryCloud} alt="Meshery Homepage" className="meshery-home" />
+          <DiscussCallout />
+        </div>
+        <div className="form-container">
+          <h3 className="form-title">Apply For the Beta Program</h3>
+          <Formik
+            initialValues={{
+              firstname: firstname,
+              lastname: lastname,
+              email: email,
+              occupation: occupation,
+              org: org,
+              google: google,
+              github: github,
+              twitter: twitter,
+              linkedin: linkedin,
+              role: role,
+              form: "play",
+            }}
+            onSubmit={values => {
+              if ((values.google || values.github || values.twitter || values.linkedin) && values.role) {
+                setMemberFormOne(values);
+                setSubmit(true);
+              } else {
+                if (!values.role) {
+                  setValidateRole(true);
+                } else {
+                  setValidateRole(false);
+                }
+                if (!(values.google || values.github || values.twitter || values.linkedin)) {
+                  setValidateAccounts(true);
+                } else {
+                  setValidateAccounts(false);
+                }
+                setFirstName(values.firstname);
+                setEmail(values.email);
+                setLastName(values.lastname);
+                setOccupation(values.occupation);
+                setOrg(values.org);
+                setGoogleAccount(values.google);
+                setTwitterAccount(values.twitter);
+                setGithubAccount(values.github);
+                setLinkedinAccount(values.linkedin);
+                setRole(values.role);
+              }
+            }}
+          >
+            <Form className="form1" method="post">
+              <div className="form-group">
+                <label htmlFor="firstname" className="form-field">First Name <span className="required-sign">*</span></label>
+                <Field type="text" className="text-field" id="firstname" name="firstname" maxLength="32"  pattern="[A-Za-z]{1,32}" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="lastname" className="form-field">Last Name <span className="required-sign">*</span></label>
+                <Field type="text" className="text-field" id="lastname" name="lastname" maxLength="32"  pattern="[A-Za-z]{1,32}" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email" className="form-field">Email Address <span className="required-sign">*</span></label>
+                <Field type="email" className="text-field" id="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="occupation" className="form-field">Occupation / Title <span className="required-sign">*</span></label>
+                <Field type="text" className="text-field" id="occupation" name="occupation" />
+                </div>
+                <div className="form-group">
+                <label htmlFor="org" className="form-field">Org / Company / School <span className="required-sign">*</span></label>
+                <Field type="text" className="text-field" id="org" name="org" />
+              </div>
+              <div className="accounts">
+                <label className="form-field">Account(s) to Connect</label>
+                <p>
+                  Choose between Twitter, Google, LinkedIn, and GitHub, provide the username/handle of your user account for your preferred identity provider. When enrolled, participants will receive a free Meshery Cloud account and have full access to the Meshery Playground early access program for each of the following user accounts that you provide. Please provide at least one account.
+                </p>
+                {validateAccounts && <p style={{ margin: "0px", color: "red" }}>{errorAccounts}</p>}
+                <div className="accounts_group">
+                    <label htmlFor="google" className="form-name">Google</label>
+                    <Field type="email" className="text-field" id="google" name="google" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
+                    <label htmlFor="github" className="form-name">GitHub</label>
+                    <Field type="text" className="text-field" id="github" name="github" />
+                    <label htmlFor="twitter" className="form-name">Twitter</label>
+                    <Field type="text" className="text-field" id="twitter" name="twitter" />
+                    <label htmlFor="linkedin" className="form-name">Linkedin</label>
+                    <Field type="text" className="text-field" id="linkedin" name="linkedin" />
+                  </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="role" className="form-field">What role best identifies you? <span className="required-sign">*</span></label>
+              </div>
+              {validateRole && <p style={{ margin: "0px", color: "red", fontSize: "12px" }}>{errorRole}</p>}
+              <div className="form-select"  aria-labelledby="select">
+                <Field as="select" name="role">
+                  <option>Select your role</option>
+                  <option value="Architect">Architect</option>
+                  <option value="Business Operations">Business Operations</option>
+                  <option value="Developer">Developer</option>
+                  <option value="DevOps">DevOps</option>
+                  <option value="Executive">Executive</option>
+                  <option value="IT Operations">IT Operations</option>
+                  <option value="Media">Media</option>
+                  <option value="Product Manager">Product Manager</option>
+                  <option value="Professor">Professor</option>
+                  <option value="Sales Marketing">Sales Marketing</option>
+                  <option value="Student">Student</option>
+                  <option value="Other">Other</option>
+                </Field>
+              </div>
+              <button type="submit" className="submit-btn">Submit</button>
+            </Form>
+          </Formik>
+        </div>
+      </div>
+    );
+  };
+
+  const ThankYou = () => {
+    return (
+      <>
+        <div className="thankyou-box">
+          <h2>Thank you for your interest in Meshery Playground early access program!</h2>
+          <p>You are now signed up for the Meshery Playground early access program and your position on the waiting list is confirmed. Please wait patiently for a response from the Meshery team.</p>
+          <p>If you have any questions in the meantime, please send an email to <a href="mailto:meshmap@layer5.io">meshmap@layer5.io</a>.</p>
+          <h3 className="white">- Team <img src={MesheryText} alt="Meshery" /></h3>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <SignupFormWrapper>
+        {
+          !formSubmitted ? <PlayFormComponent /> : <ThankYou /> 
+        }
+    </SignupFormWrapper>
+  );
 };
 
 export default SignupForm;
