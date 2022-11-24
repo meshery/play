ff381e3eebc029b2fc07488a0450f798a8c4aa39
Nikhil Ladha <nikhilladha1999@gmail.com>
2022-11-24 23:29:32 +0530

    test
    Signed-off-by: Nikhil Ladha <nikhilladha1999@gmail.com>

--- site/src/index.js
+++ site/src/index.js
@@ -13,7 +13,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your app, pass a functions
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
