48e91b041da72b5b0fc26af65f0651178a6d2462
Lee Calcote <lee.calcote@layer5.io>
2023-04-22 17:44:01 -0500

    more faqs
    Signed-off-by: Lee Calcote <lee.calcote@layer5.io>

--- site/src/assets/data/faq/index.js
+++ site/src/assets/data/faq/index.js
@@ -1,14 +1,70 @@
+/* eslint-disable no-multi-str */
 const data = {
     faqs: [
+
+// Copy and paste the following template 
+// for new Q/A entries 
+/*
+        {
+            question: "",
+            category: "Meshery Playground",
+            answer: [
+                "",
+                "",
+            ],
+        },
+*/
+
+        {
+            question: "Why do I need to sign-up/register to use the Cloud Native Playground?",
+            category: "Meshery Playground",
+            answer: [
+                "Sign-up to receive a user account, entitled with perpetually free access, because the Playground is not a crypto mine. The Meshery Playground is connected to live Kubernetes cluster(s) and allows users full-control over those clusters. ",
+                "Requiring users to sign up  and register a user account keeps the Playground a safe and healthy for all to enjoy. Without requiring users to sign-in the Meshery Playground would allow anonymous access for anyone and everyone to potentially perform nefarious acts. ",
+            ],
+        },
+        {
+            question: "Will I lose my work in the Cloud Native Playground?",
+            category: "Meshery Playground",
+            answer: [
+                "No, all of the designs that you have created in the Meshery Playground are saved to your user account and will be available to you each time that you sign into your account. Your designs will be available in the Meshery Playground and any your other Meshery deployments, too.",
+                "Yes, the Meshery Playground is reset on a daily basis. However, only deployments that you may have created will be lost (e.g.only MeshMap Visualizer is reset). All of your work in Meshery UI and MeshMap Designer will remain in your user account.",
+            ],
+        },
+        {
+            question: "Is Meshery open source?",
+            category: "Meshery Playground",
+            answer: [
+                "Yes, Meshery is a Cloud Native Computing Foundation (CNCF) project and is licensed under Apache v2.",
+                "As the extensible cloud native manager, Meshery is a pluggable platform, offering multiple extension points within which users and partners (ISVs) can customize and extend Meshery's functionality under the same or different license(s), offering commercial and non-commericial extensions.",
+            ],
+        },
         {
             question: "What is the difference between the Cloud Native Playground and MeshMap?",
             category: "Meshery Playground",
             answer: [
                 "The Cloud Native Playground (aka Meshery Playground) is a managed instance of Meshery that offers a sandbox environment in which half of MeshMap functionality is enabled (Designer mode) and the other half of MeshMap functionality is disabled (Visualizer mode).",
-                "The sandbox environment is not connected to an active Kuberentes cluster, and as such, specific actions within MeshMap Designer are also disabled. Meshery and MeshMap are feature-rich, sophisticated management applications for cloud native infrastructure. To access their full set of capabilities, simply deploy your own copy of Meshery into the environment of your choosing.",
+                "The sandbox environment is not connected to an active Kuberentes cluster, and as such, depending upon which Meshery Playgrouns environment you use specific actions within MeshMap Designer are also disabled. Meshery and MeshMap are feature-rich, sophisticated management applications for cloud native infrastructure. To access their full set of capabilities, simply deploy your own copy of Meshery into the environment of your choosing.",
+            ],
+        },
+        {
+            question: "Are designs that I create in the Meshery Playground saved or will I look any work that I do in the playground?",
+            category: "Meshery Playground",
+            answer: [
+                "Yes, all of the designs that you have created in the Meshery Playground are saved to your user account and will be available to you each time that you return to the Playground. The designs will also be available in your other Meshery deployments.",
+            ],
+        },
+        {
+            question: "Can I deploy the Meshery Playground (MeshMap) on-premises?",
+            category: "Meshery Playground",
+            answer: [
+                "Yes, Meshery can be deployed on-premises with a single command to download, install, and run your own instance of Meshery in your environment.",
+                "See all supported platforms to deploy your own Meshery instance at https://meshery.io",
             ],
         },
     ]
 }
 
+
+ 
 export default data;
