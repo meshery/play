7863cbf2eb4ead64218393b5823b18faec067d51
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-12-26 23:51:39 +0530

    fix:eslinterror
    Signed-off-by: saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>

--- site/src/components/Discuss-Callout/index.js
+++ site/src/components/Discuss-Callout/index.js
@@ -9,7 +9,7 @@ const DiscussCallout = () => {
       <div className="explain">
         <div className="cards">
           <div className="card">
-            <a href="http://discuss.meshery.io/" target="_blank">
+            <a href="http://discuss.meshery.io/" target="_blank" rel="noreferrer">
               <div className="parentcard">
                 <div className="section-title">
                   <h2>Join the Conversation</h2>
